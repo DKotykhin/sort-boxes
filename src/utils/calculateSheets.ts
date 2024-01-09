19140d173d033c3f9cd11a51bb4305974c5c10b7
Dmytro Kotykhin <kotykhin_d@ukr.net>
2024-01-10 00:28:51 +0100

    boxes calculation update

--- src/utils/calculateSheets.ts
+++ src/utils/calculateSheets.ts
@@ -1,35 +1,38 @@
 import { Block, Sheet } from 'constants/blocks';
 
 export const calculateSheets = (blocks: Block[], sheet: Sheet) => {
-    const sortedBlocks = [...blocks].sort((a, b) => b.width * b.height - a.width * a.height);
-    // console.log('sortedBlocks: ', sortedBlocks)
-
     let usedSheets = 0;
+    let sheetHeight = sheet.height;
 
-    for (const block of sortedBlocks) {
+    for (const block of blocks) {
         let blockQuantity = block.quantity;
-        // console.log('outer counter ', usedSheets)
+        // console.log('outer counter ', usedSheets);
 
         const blocksFitHorizontally = Math.floor(sheet.width / block.width);
-        const blocksFitVertically = Math.floor(sheet.height / block.height);
-        const maxBlocks = blocksFitHorizontally * blocksFitVertically;
-        // console.log('maxBlocks: ', maxBlocks);
+        const blocksFitVertically = Math.floor(sheetHeight / block.height);
+        let maxBlocks = blocksFitHorizontally * blocksFitVertically;
+        sheetHeight = sheet.height;
 
         for (let i = 0; i < usedSheets && blockQuantity > 0; i++) {
-            // console.log('inner counter', usedSheets)
+            // console.log('inner counter', usedSheets);
 
             const blocksToPlace = Math.min(maxBlocks, blockQuantity);
             blockQuantity -= blocksToPlace;
 
-            if (blockQuantity > 0) {
-                usedSheets++;
-            }
+            const blocksFitVertically = Math.floor(sheet.height / block.height);
+            maxBlocks = blocksFitHorizontally * blocksFitVertically;
+
+            if (blockQuantity > 0) usedSheets++;
         }
 
         while (blockQuantity > 0) {
             usedSheets++;
-            blockQuantity -=
-                Math.floor(sheet.width / block.width) * Math.floor(sheet.height / block.height);
+            blockQuantity -= maxBlocks;
+            if (blockQuantity === 0) usedSheets++;
+            if (blockQuantity <= 0) {
+                sheetHeight =
+                    block.height * Math.floor(Math.abs(blockQuantity) / blocksFitHorizontally);
+            }
         }
     }
 
